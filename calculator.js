3ffb633a719bb431cb21e9f2efb3963f6d563329
MK410668 <mateusz.qzera@gmail.com>
2024-03-14 10:06:20 +0100

    added

--- calculator.js
+++ calculator.js
@@ -18,4 +18,9 @@ function div(a, b) {
     return a / b;
 }
 
+console.log("5 + 3 = " +  add(5, 3));
+console.log("10 - 4 = " +sub(10, 4));
+console.log("7 * 2 = " +multiply(7, 2));
+console.log("20 / 5 = " +div(20, 5));
+
 module.exports = { add, sub, multiply, div };
